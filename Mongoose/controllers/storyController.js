ddf1405e43ddcf8354844dcacb33889dba9e0d2d
Alice Chen <achen24@charlotte.edu>
2025-03-21 15:58:53 -0400

    update functions to be asychronous

--- Mongoose/controllers/storyController.js
+++ Mongoose/controllers/storyController.js
@@ -1,8 +1,9 @@
 const model = require('../models/story');
-exports.index = (req, res)=>{
+exports.index = (req, res, next)=>{
     //res.send('send all stories');
-    let stories = model.find();
-    res.render('./story/index', {stories});
+    model.find()
+    .then(stories=>res.render('./story/index', {stories}))
+    .catch(err=>next(err));
 };
 
 exports.new = (req, res)=>{
@@ -24,49 +25,92 @@ exports.create = (req, res, next)=>{
 
 exports.show = (req, res, next)=>{
     let id = req.params.id;
-    let story = model.findById(id);
-    if(story) {
-        res.render('./story/show', {story});
-    } else {
-        let err = new Error('Cannot find a story with id ' + id);
-        err.status = 404;
-        next(err);
+    //an objectId is a 24 bit hex string
+    if (!id.match(/^[0-9a-fA-f]{24}$/)) {
+        let err = new Error('Invalid story id: ' + id);
+        err.status = 400;
+        return next(err);
     }
-   
+
+    model.findById(id)
+    .then(story => {
+        if(story) {
+            return res.render('./story/show', {story});
+        } else {
+            let err = new Error('Cannot find a story with id ' + id);
+            err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err => next(err));
 };
 
 exports.edit = (req, res, next)=>{
     let id = req.params.id;
-    let story = model.findById(id);
-    if(story) {
-        res.render('./story/edit', {story});
-    } else {
-        let err = new Error('Cannot find a story with id ' + id);
-        err.status = 404;
-        next(err);
+    if (!id.match(/^[0-9a-fA-f]{24}$/)) {
+        let err = new Error('Invalid story id: ' + id);
+        err.status = 400;
+        return next(err);
     }
+
+    model.findById(id)
+    .then(story => {
+        if(story) {
+            return res.render('./story/edit', {story});
+        } else {
+            let err = new Error('Cannot find a story with id ' + id);
+            err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err => next(err));
 };
 
 exports.update = (req, res, next)=>{
     let story = req.body;
     let id = req.params.id;
 
-   if (model.updateById(id, story)) {
-       res.redirect('/stories/'+id);
-   } else {
-    let err = new Error('Cannot find a story with id ' + id);
-        err.status = 404;
+    if (!id.match(/^[0-9a-fA-f]{24}$/)) {
+        let err = new Error('Invalid story id: ' + id);
+        err.status = 400;
+        return next(err);
+    }
+
+    model.findByIdAndUpdate(id, story, {useFindAndModify: false, runValidators: true})
+    .then(story =>{
+        if(story) {
+            res.redirect('/stories/' + id);
+        } else {
+            let err = new Error('Cannot find a story with id ' + id);
+            err.status = 404;
+            next(err)
+        }
+    })
+    .catch(err => {
+        if(err.name === 'ValidationError')
+            err.status = 400;
         next(err);
-   }
+    });
 };
 
 exports.delete = (req, res, next)=>{
     let id = req.params.id;
-    if(model.deleteById(id))
-        res.redirect('/stories');
-    else {
-        let err = new Error('Cannot find a story with id ' + id);
-        err.status = 404;
-        next(err);
+
+    if (!id.match(/^[0-9a-fA-f]{24}$/)) {
+        let err = new Error('Invalid story id: ' + id);
+        err.status = 400;
+        return next(err);
     }
+
+    model.findByIdAndDelete(id, {useFindAndModify: false})
+    .then(story => {
+        if(story) {
+            res.redirect('/stories');
+        } else {
+            let err = new Error('Cannot find a story with id ' + id);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
 };
